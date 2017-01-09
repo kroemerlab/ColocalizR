901cfa2719c4060b772d9434ec385a0b05d7f624
Allan <allan.sauvat@gmail.com>
2017-01-09 15:00:06 +0100

    Update pkgs.R

--- ColocalizR/3rd-part/pkgs.R
+++ ColocalizR/3rd-part/pkgs.R
@@ -10,7 +10,8 @@ if(!dir.exists(libdir)){dir.create(libdir)}
 .libPaths(new = libdir)
 shell(paste('setx R_LIBS_USER ',libdir,sep=''))
 #
-source("http://bioconductor.org/biocLite.R")
-biocLite(pkgs=c('EBImage','flowCore'), ask=F)
 install.packages(pkgs = c('shiny','tiff','reshape','RODBC','foreach','doParallel','stringi','naturalsort','rChoiceDialogs'),
                  lib = libdir, repos = "http://cloud.r-project.org")
+source("http://bioconductor.org/biocLite.R")
+biocLite(pkgs=c('EBImage','flowCore'), ask=F, lib = libdir)
+
